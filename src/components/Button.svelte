60cab0ed549f3ad7240ce7458eae69b9f40f0770
Gautierpicon <gautierpicon@proton.me>
2025-11-11 17:21:22 +0100

    first commit

---
+++ src/components/Button.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let type = "button";
+</script>
+
+<button
+  type={type}
+  class="cursor-pointer px-4 py-2 rounded-lg font-medium transition bg-blue-600 text-white hover:bg-blue-700"
+>
+  <slot />
+</button>
